bcb0ddef0e71f558f72d7040458c10c57219af8f
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-29 19:23:05 +0545

    Routes Logic

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/notuser',function()
+{
+    return "<h1>String<h2>";
+});
+
+
+Route::post('/', function (Request $request) {
+    dd($request->all());
+});
+
+
+
+Route::put('/{id}', function (Request $request, int $id) {
+    return $id . " " . $request->username;
+});
+
+Route::delete('/{id}', function (int $id) {
+    return "Deleted " . $id;
+}
+);
+
+
