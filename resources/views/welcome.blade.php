bcd53497f2e932042ff585d866a58eb9e602e1b9
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-03 20:53:06 +0545

    Components Setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>Hello World !</h1>
-
-<form action="/" method="POST">
-
-    @csrf
-    <input type="text" name="username" id="username">
-    <button type="submit">Submit</button>
-
-</form>
-
-<form action="/999999999" method="POST">
-    @csrf
-    @method('delete')
-    <button type="delete">DELETE</button>
-</form>
-
-<form action="/999" method="POST">
-    @csrf
-    <input type="hidden"  name = "_method" value="PUT">
-    <input type="text" name="username" id="username">
-    <button type="submit">Submit PUT</button>
-</form>
-
-
-</body>
-</html>
+<x-layout>
+    Hello World!
+    <p class="bg-blue-500">Welcome TO This Component Layout Page</p></x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('posts.index');
+    return view('welcome');
 });
 
 
