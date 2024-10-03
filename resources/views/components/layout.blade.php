fe48cc6b263578f779d7632cfb85f9d025b28d50
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-03 21:03:31 +0545

    Vite Used Properly

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,8 +7,24 @@ CTYPE html>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="../css/app.css">
+    @vite('resources/css/app.css')
 </head>
 <body>
+    <nav>
+        <div class="flex justify-between items-center container mx-auto bg-blue-600 text-white text-lg">
+
+
+
+            <a>Home</a>
+            <a>About</a>
+            <a>Services</a>
+            <a>Posts</a>
+            <a>Contact us</a>
+
+
+
+        </div>
+    </nav>
     {{ $slot }}
 </body>
 </html>
