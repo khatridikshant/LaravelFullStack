b567f9625e51e0d97de7b80c90c9db2d56e10cb8
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-11 08:20:55 +0545

    Form FOr Post Create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-    <link rel="stylesheet" href="../css/app.css">
-</head>
-<body>
-    <h1>Create A Post</h1>
-
-</body>
-</html>
+<x-layout>
+<div class="max-w-2xl">
+<form class="bg-gray-400 p-2 rounded-sm">
+  <div class="mb-5">
+    <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+    <input type="text" id="title" class="bg-gray-50 border
+     border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
+      focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
+       dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Title" required />
+  </div>
+    <div class="mb-5">
+      <label for="your-message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Message</label>
+      <input type="text" id="your-message" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+  </div>
+  <button type="submit"
+   class="text-white bg-blue-700
+    hover:bg-blue-800 focus:ring-4
+     focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2
+      dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+      Submit</button>
+</form>
+</div>
+</x-layout>
