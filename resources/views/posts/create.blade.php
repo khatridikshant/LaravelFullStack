ded0ca8a80ce560a4d31510b549c1fc03907a24a
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-13 19:28:54 +0545

    Error Display Fixed

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,21 +10,26 @@
   <div class="mb-5">
     <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
     <input
-     type="text" name="title" id="title" class="bg-gray-50 border
+     type="text" name="title" id="title" class="@error('title')
+     border-red-500
+     @enderror
+     bg-gray-50 border
      border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
       focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
        dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Title" required />
+        placeholder="Title" />
         @error('title')
           <span class="text-red-500">{{$message}}</span>
         @enderror
   </div>
     <div class="mb-5">
       <label for="postcontent" name="postcontent" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Message</label>
-      <input type="text" name="postcontent" id="postcontent" class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  @error('postcontent')
-    <span class="text-red-500">{{$message}}</span>
-  @enderror
+      <input
+       type="text" name="postcontent" id="postcontent" 
+      class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+@error('content')
+      <span class="text-red-500">{{$message}}</span>
+      @enderror
   </div>
   <button type="submit"
    class="text-white bg-blue-700
