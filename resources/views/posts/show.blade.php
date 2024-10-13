669f9534899ff7adceed078deb64ec9172b19ad4
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-13 20:51:54 +0545

    Specific Post DisplayPage

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,7 @@
-<x-layout><h1>Posts Display</h1>
-<div>{{$post->title}}</div>
+<x-layout>
+<div class=" bg-blue-300 h-2/4 p-5">
+<div class="text-lg">{{$post->title}}</div>
+<div class="text-5xl">{{$post->content}}</div>
+<div class="text-lg mt-10">{{$post->created_at}}</title>
+</div>
 </x-layout>
