06e0ee78d015d1ac6d4e5203b9ee5a2cb58d81d1
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-13 20:40:48 +0545

    Posts Display Design

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,23 +12,29 @@
     <div class="grid grid-cols-3 gap-5">
         @foreach ($posts as $key)
 
-            <div
-                class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $key->title }}
-                    </h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $key->content }}</p>
-                <a href="/posts/{{$key->id}}"
-                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9" />
-                    </svg>
-                </a>
-            </div>
+
+<div class="flex flex-col bg-white border shadow-sm rounded-xl">
+  <div class="p-4 md:p-5">
+    <h3 class="text-lg font-bold text-gray-800">
+{{$key->title}}
+    </h3>
+    <p class="mt-2 text-gray-500">
+{{$key->content}}
+    </p>
+    <a href="/posts/{{$key->id}}" class="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 decoration-2 hover:text-blue-700 hover:underline focus:underline focus:outline-none focus:text-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
+      Card link
+      <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <path d="m9 18 6-6-6-6"></path>
+      </svg>
+    </a>
+  </div>
+  <div class="bg-gray-100 border-t rounded-b-xl py-3 px-4 md:py-4 md:px-5">
+    <p class="mt-1 text-sm text-gray-500">
+    {{$key->created_at}}
+    </p>
+  </div>
+</div>
+
         @endforeach
     </div>
     </div>
