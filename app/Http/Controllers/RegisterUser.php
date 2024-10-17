0f224f9201d0cfb98c70640f05cbf5e812b5232f
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-17 15:30:26 +0545

    Registration Working

--- app/Http/Controllers/RegisterUser.php
+++ app/Http/Controllers/RegisterUser.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterUser extends Controller
 {
@@ -12,13 +16,25 @@ class RegisterUser extends Controller
     }
 
     public function store(Request $request){
+       
         $request->validate(
             [
                 'name' => 'required|min:5|max:255|string',
-                'email' => 'required|email|unique:users',
-                'password' => '<PASSWORD>',
+                'email' => 'required|email|unique:users,email',
+                'password' => ['required','min:8','confirmed',Password::default()],
             ]
         );
+
+        $user = User::create([
+            'name'=> $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        FacadesAuth::login($user);
+
+        return  to_route('posts.index');
+
     }
     
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,13 +4,14 @@
     </h1>
 
     <div class="px-5 py-5 bg-slate-800 rounded-md m-2">
-            <form class=" flex-col bg-zinc-500 rounded-sm px-5 py-5 md:mt-2" action="#">
+            <form class=" flex-col bg-zinc-500 rounded-sm px-5 py-5 md:mt-2" action="{{route('register.store')}}" method="POST">
+                @csrf
                 <div class="m-2">
                     <label for="name" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Your
                         Name</label>
                     <input type="text" name="name" id="name"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@company.com" required="">
+                        placeholder="Full Name" required="">
                         @error('name')
                         <span class="text-red-600">{{$message}}</span>                   
                         @enderror
@@ -39,14 +40,14 @@
                 <div class="m-2">
                     <label for="confirm-password"
                         class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Confirm password</label>
-                    <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••"
+                    <input type="password" name="password_confirmation" id="confirm-password" placeholder="••••••••"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required="">
-                        @error('confirm-password')
+                        @error('password_confirmation')
                         <span class="text-red-600">{{$message}}</span>
                         @enderror
                 </div>
-                <button type="button"
+                <button type="submit"
                     class=" mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Register</button>
             </form>
 </div>
