bad237560d7a0f3de403baf11515de16257b9368
Dikshant Khatri <khatridikshant@outlook.com>
2024-10-17 14:59:48 +0545

    Registration

--- app/Http/Controllers/RegisterUser.php
+++ app/Http/Controllers/RegisterUser.php
@@ -10,5 +10,15 @@ class RegisterUser extends Controller
     public function register(){
         return view('auth.register');
     }
+
+    public function store(Request $request){
+        $request->validate(
+            [
+                'name' => 'required|min:5|max:255|string',
+                'email' => 'required|email|unique:users',
+                'password' => '<PASSWORD>',
+            ]
+        );
+    }
     
 }
